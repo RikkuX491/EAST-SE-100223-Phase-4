084ce4643f7f939d866fc63e1086f153aed62b79
Ricardo Guerra <RikkuG491@gmail.com>
2023-12-06 09:16:17 -0500

    Updated seed.py code

--- server/seed.py
+++ server/seed.py
@@ -7,18 +7,11 @@ with app.app_context():
 
     Hotel.query.delete()
 
-    hotels = []
-
     hotel1 = Hotel(name = "Marriott")
-    hotels.append(hotel1)
-
     hotel2 = Hotel(name = "Hampton Inn")
-    hotels.append(hotel2)
-
     hotel3 = Hotel(name = "Hilton Resort")
-    hotels.append(hotel3)
 
-    db.session.add_all(hotels)
+    db.session.add_all([hotel1, hotel2, hotel3])
     db.session.commit()
 
     print("🌱 Hotels successfully seeded! 🌱")
