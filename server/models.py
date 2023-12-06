eed11c206de3a8cb3bfcaaabf5a558df38de04fd
Ricardo Guerra <RikkuG491@gmail.com>
2023-12-06 08:58:14 -0500

    Updated naming_convention for MetaData in models.py

--- server/models.py
+++ server/models.py
@@ -2,9 +2,17 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
 # contains definitions of tables and associated schema constructs
 # read more about Metadata using the link at the bottom of the page
-metadata = MetaData()
+metadata = MetaData(naming_convention=convention)
 
 # create the Flask SQLAlchemy extension
 db = SQLAlchemy(metadata=metadata)
