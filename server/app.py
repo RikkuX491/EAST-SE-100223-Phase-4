dd181040404aaa07440dde9a30d0628f70cb094a
Ricardo Guerra <RikkuG491@gmail.com>
2023-12-07 12:06:49 -0500

    Added solution code

--- server/app.py
+++ server/app.py
@@ -22,29 +22,77 @@ migrate = Migrate(app, db)
 # initialize the Flask application to use the database
 db.init_app(app)
 
-@app.route('/hotels')
-def get_hotels():
-    hotels = Hotel.query.all()
+api = Api(app)
 
-    response_body = []
+class AllHotels(Resource):
 
-    for hotel in hotels:
-        response_body.append(hotel.to_dict(only=('id', 'name')))
-        
-    return make_response(response_body, 200)
+    def get(self):
+        hotels = Hotel.query.all()
 
-@app.route('/hotels/<int:id>')
-def hotel_by_id(id):
-    hotel = Hotel.query.filter(Hotel.id == id).first()
+        response_body = []
 
-    if hotel:
-        response_body = hotel.to_dict(only=('id', 'name', 'reviews.id', 'reviews.rating', 'reviews.hotel_id', 'reviews.customer_id'))
+        for hotel in hotels:
+            response_body.append(hotel.to_dict(only=('id', 'name')))
+            
         return make_response(response_body, 200)
-    else:
-        response_body = {
-            'error': 'Hotel Not Found'
-        }
-        return make_response(response_body, 404)
+    
+    def post(self):
+        new_hotel = Hotel(name=request.json.get('name'))
+        db.session.add(new_hotel)
+        db.session.commit()
+        response_body = new_hotel.to_dict(only=('id', 'name'))
+        return make_response(response_body, 201)
+
+api.add_resource(AllHotels, '/hotels')
+
+class HotelById(Resource):
+
+    def get(self, id):
+        hotel = Hotel.query.filter(Hotel.id == id).first()
+
+        if hotel:
+            response_body = hotel.to_dict(only=('id', 'name', 'reviews.id', 'reviews.rating', 'reviews.hotel_id', 'reviews.customer_id'))
+            response_body['customers'] = [customer.to_dict(only=('id', 'first_name', 'last_name')) for customer in list(set(hotel.customers))]
+            return make_response(response_body, 200)
+        else:
+            response_body = {
+                'error': 'Hotel Not Found'
+            }
+            return make_response(response_body, 404)
+    
+api.add_resource(HotelById, '/hotels/<int:id>')
+
+# @app.route('/hotels', methods=["GET", "POST"])
+# def all_hotels():
+    # if request.method == 'GET':
+    #     hotels = Hotel.query.all()
+
+    #     response_body = []
+
+    #     for hotel in hotels:
+    #         response_body.append(hotel.to_dict(only=('id', 'name')))
+            
+    #     return make_response(response_body, 200)
+    
+    # elif request.method == 'POST':
+    #     new_hotel = Hotel(name=request.json.get('name'))
+    #     db.session.add(new_hotel)
+    #     db.session.commit()
+    #     response_body = new_hotel.to_dict(only=('id', 'name'))
+    #     return make_response(response_body, 201)
+
+# @app.route('/hotels/<int:id>')
+# def hotel_by_id(id):
+#     hotel = Hotel.query.filter(Hotel.id == id).first()
+
+#     if hotel:
+#         response_body = hotel.to_dict(only=('id', 'name', 'reviews.id', 'reviews.rating', 'reviews.hotel_id', 'reviews.customer_id'))
+#         return make_response(response_body, 200)
+#     else:
+#         response_body = {
+#             'error': 'Hotel Not Found'
+#         }
+#         return make_response(response_body, 404)
 
 @app.route('/customers')
 def get_customers():
@@ -77,9 +125,22 @@ def get_reviews():
     response_body = []
 
     for review in reviews:
-        response_body.append(review.to_dict(rules=('-hotel.reviews', '-customer.reviews')))
+        response_body.append(review.to_dict(rules=('-hotel', '-customer')))
 
     return make_response(response_body, 200)
+
+@app.route('/reviews/<int:id>')
+def review_by_id(id):
+    review = Review.query.filter(Review.id == id).first()
+
+    if review:
+        response_body = review.to_dict(rules=('-hotel.reviews', ('-customer.reviews')))
+        return make_response(response_body, 200)
+    else:
+        response_body = {
+            'error': 'Review Not Found'
+        }
+        return make_response(response_body, 404)
 
 if __name__ == "__main__":
     app.run(port=7777, debug=True)
